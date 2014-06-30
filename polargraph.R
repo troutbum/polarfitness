6c14bf2385df23fce7b591c4d1fdea08d5f31d45
Gene Cho <genec2008@gmail.com>
2014-06-29 20:20:26 -0700

    working version
    makes 2 individual plots and a combined plot

--- polargraph.R
+++ polargraph.R
@@ -1,47 +1,56 @@
 library(datasets)
 library(ggplot2)
 library(RColorBrewer)
-
-colors <- brewer.pal(12, "Paired")
+colors1 <- brewer.pal(9, "Greens")
+palette1 <- colorRampPalette(colors1)
+colors2 <- brewer.pal(9, "YlOrRd")
+palette2 <- colorRampPalette(colors2)
 
 # read in fitness test data
-testdata <- read.csv("~/CourseraHW/R/10AttributesFitnessData.csv")
+testdata <- read.csv("10AttributesFitnessData.csv")
 
 # create a TO DO list, inverse of test results
 todo <- testdata
 todo[,2] <- abs(todo[,2] - 10)
 
+# if a Score is 0, make it a 1
 for (i in 1:nrow(todo)) 
 {
         if (todo[i,2] == 0)  {todo[i,2] <- todo[i,2] + 1}
 }
 
 
-ggplot(todo, aes(Attribute, Score)) + geom_bar(stat="identity") + coord_polar() +
-        xlab("June 26, 2014") + ylab("") + ggtitle("TO DO List") 
+# plot of Fitness Test
+png("fittest.png", width = 960, height = 960)
+testplot <- ggplot(testdata, aes(Attribute, Score, fill = factor(Score))) + 
+        geom_bar(stat="identity") + 
+        coord_polar() +
+        theme(legend.position="none") +
+        labs(x="June 26, 2014") + 
+        labs(y="") + 
+        scale_y_continuous(limits=c(0,10)) +
+        labs(title="10 Attributes of Fitness Test Results") +     
+        scale_fill_manual(values=palette1(10)) +
+        theme(plot.title = element_text(size = rel(2), face="bold"))
+testplot
+dev.off()
 
+# plot of ToDo list (inverse of Fitness Test)
+png("todo.png", width = 960, height = 960)
+todoplot <- ggplot(todo, aes(Attribute, Score, fill = factor(Score))) + 
+        geom_bar(stat="identity") + 
+        coord_polar() +
+        theme(legend.position="none") +
+        labs(x="June 26, 2014") + 
+        labs(y="") + 
+        scale_y_continuous(limits=c(0,10)) +
+        labs(title="To Do List") +     
+        scale_fill_manual(values=palette2(10)) +
+        theme(plot.title = element_text(size = rel(2), face="bold"))
+todoplot
+dev.off()
 
-ggplot(testdata, aes(Attribute, Score)) + geom_bar(stat="identity") + coord_polar() +
-        xlab("Test Results June 26, 2014") + ylab("")
-
-
-
-# # library(RColorBrewer)
-# # cols <- brewer.pal(12, "Paired")
-
-
-png("plot6.png", width = 960, height = 960)
-gB <- ggplot(B, aes(SCC.Level.Three, Emissions, fill=SCC.Level.Three))          # Baltimore 
-gB <- gB + geom_bar(stat="identity") + facet_grid(. ~ year) 
-gB <- gB + scale_x_discrete(breaks=NULL)                                        # suppress x-axis label
-gB <- gB + xlab("") + ylab("PM2.5 Emissions (in tons)")
-gB <- gB + ggtitle("Baltimore City, Maryland | Motor Vehicle PM2.5 Emissions") 
-gB <- gB + labs(fill = "Vehicle Type")
-gLA <- ggplot(LA, aes(SCC.Level.Three, Emissions, fill=SCC.Level.Three))        # LA
-gLA <- gLA + geom_bar(stat="identity") + facet_grid(. ~ year) 
-gLA <- gLA + scale_x_discrete(breaks=NULL)                                      # suppress x-axis label
-gLA <- gLA + xlab("") + ylab("PM2.5 Emissions (in tons)")
-gLA <- gLA + ggtitle("Los Angeles County, California | Motor Vehicle PM2.5 Emissions") 
-gLA <- gLA + labs(fill = "Vehicle Type")
-multiplot(gB, gLA, cols=1)
+png("both.png", width = 960, height = 960)
+source('~/CourseraHW/R_functions/multiplot.R')
+multiplot(testplot, todoplot, cols=1)
 dev.off()
